0b4180de2ead8ab9340675aada42339b64f2c6b0
f <f@email>
2025-06-16 15:00:26 +0200

    changed site meta and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://celestialdocs.hyperoot.dev",
+  site: "https://liederbuch.goldgraben.net",
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
