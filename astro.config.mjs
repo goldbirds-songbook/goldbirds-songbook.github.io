b1c4c700c8a27e297b62a0b3b1392aa127f0c81c
f <f@email>
2025-06-17 12:34:57 +0200

    adjusted settings for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://goldbirds-songbook.github.io",
+  base: "/",
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
